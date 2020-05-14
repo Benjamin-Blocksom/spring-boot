f21c2ad347791c83636ef20c28796315d98a6413
AbraCadabstrax <benjamin.blocksom@gmail.com>
2020-05-14 05:55:47 -0400

    Initialize starter web application with Spring boot. HelloController maps to a *Hello World* page and main method in Application lists Spring resources. This can be used as a starter for future Spring-Boot projects.

---
+++ src/main/java/online/basicbuddy/springboot/HelloController.java
@@ -0,0 +1,14 @@
+package online.basicbuddy.springboot;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RestController
+public class HelloController {
+
+	@RequestMapping("/")
+	public String index() {
+		return "Jello Whirled: Spring Boot Flavor";
+	}
+
+}
